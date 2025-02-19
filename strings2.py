cf210b5cef3d90e664265c0ebf88182451bd4bbb
Jonathan C <jcarrico1985@live.com>
2025-02-18 18:29:00 -0600

    add strings2.py for lesson 29 the str string data type

---
+++ strings2.py
@@ -0,0 +1,11 @@
+#         012345678901234
+parrot = "Norwegian Blue"
+
+print(parrot)
+
+print(parrot[3]) # w
+print(parrot[4]) # e
+print(parrot[9]) # ' '
+print(parrot[3]) # w
+print(parrot[6]) # i
+print(parrot[8]) # n
