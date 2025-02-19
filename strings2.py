eb6a06241f9813c101f8df0f24966410121984ff
Jonathan C <jcarrico1985@live.com>
2025-02-18 23:01:40 -0600

    edit strings2.py for lesson 31 slicing

--- strings2.py
+++ strings2.py
@@ -9,3 +9,26 @@ print(parrot[9]) # ' '
 print(parrot[3]) # w
 print(parrot[6]) # i
 print(parrot[8]) # n
+
+print(parrot[0:6]) # Norweg
+print(parrot[6:])
+
+print(parrot[3:5])
+print(parrot[0:9]) # Norwegian
+print(parrot[:9]) # Norwegian - prints the same thing as providing '0' so we don't need to provide it.
+print(parrot[10:]) # Blue - do not need to provide ending index if going to end of string.
+print(parrot[:]) # Norwegian Blue
+
+#          01234567890123456789012345
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+name = 'bartholomew'
+copied_name = ''
+
+# a very direct way of spelling out a name with the alphabet string
+# print(letters[9] + letters[14] + letters[13] + letters[0] + letters[19] + letters[7] + letters[0] + letters[13])
+
+# my very roundabout way of copying the name to another variable by using the alphabet string.
+for character in name:
+    copied_name += alphabet[alphabet.index(character)]
+
+print(copied_name.capitalize())
